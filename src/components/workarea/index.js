fd07686b57529863b53d1fc8db0681e1bc91d4fc
Karan Nehra <karanehra@gmail.com>
2020-08-04 23:07:40 +0530

    Cleanup and removed position change event bubble

--- src/components/workarea/index.js
+++ src/components/workarea/index.js
@@ -1,71 +1,69 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import TreeNode from "../../shared/node/";
-import { setActiveNodeAction } from "../../redux/actions/nodeActions";
-import Connector from "../../shared/connector/";
-import WorkareaToolbar from "../workareaToolbar/index";
-import "./workarea.scss";
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import TreeNode from '../../shared/node/'
+import { setActiveNodeAction } from '../../redux/actions/nodeActions'
+import Connector from '../../shared/connector/'
+import './workarea.scss'
 
 const WorkArea = () => {
-  const [workSpaceHeight, changeHeight] = useState(window.innerHeight - 50);
-  const [workSpaceWidth, changeWidth] = useState(window.innerWidth);
+  const [workSpaceHeight, changeHeight] = useState(window.innerHeight - 50)
+  const [workSpaceWidth, changeWidth] = useState(window.innerWidth)
 
   let { nodes, conn, translateX, translateY } = useSelector(state => ({
     nodes: state.nodeReducer.nodes,
     conn: state.connectorReducer.connections,
     translateX: state.workspaceReducer.translateX,
-    translateY: state.workspaceReducer.translateY
-  }));
+    translateY: state.workspaceReducer.translateY,
+  }))
 
-  const [connections, updateConnections] = useState(conn);
+  const [connections, updateConnections] = useState(conn)
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   window.onresize = () => {
-    changeHeight(window.innerHeight - 50);
-    changeWidth(window.innerWidth);
-  };
+    changeHeight(window.innerHeight - 50)
+    changeWidth(window.innerWidth)
+  }
 
   const removeActiveNode = () => {
-    dispatch(setActiveNodeAction(null));
-  };
+    dispatch(setActiveNodeAction(null))
+  }
 
   const handlePositionUpdate = (nodeID, cx, cy) => {
-    let p = [...connections];
+    let p = [...connections]
     if (p.length) {
       for (let i = 0; i < p.length; i++) {
         if (p[i].id1 === nodeID) {
           p[i] = {
             ...p[i],
             x1: Number(cx),
-            y1: Number(cy)
-          };
+            y1: Number(cy),
+          }
         } else if (connections[i].id2 === nodeID) {
           p[i] = {
             ...p[i],
             x2: Number(cx),
-            y2: Number(cy)
-          };
+            y2: Number(cy),
+          }
         }
       }
     }
-    updateConnections(p);
-  };
+    updateConnections(p)
+  }
 
   return (
     <React.Fragment>
-      <WorkareaToolbar />
       <svg
         width={workSpaceWidth}
         height={workSpaceHeight}
         onClick={removeActiveNode}
-        className="workarea"
+        className='workarea'
       >
         <g transform={`translate(${translateX},${translateY})`}>
           {connections.length > 0 &&
             connections.map((connection, i) => {
-              console.log("hmm k");
-              return <Connector {...connection} key={i} />;
+              console.log('hmm k')
+              return <Connector {...connection} key={i} />
             })}
           {nodes.length > 0 &&
             nodes.map(node => (
@@ -80,7 +78,7 @@ const WorkArea = () => {
         </g>
       </svg>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default WorkArea;
+export default WorkArea
--- src/shared/node/index.js
+++ src/shared/node/index.js
@@ -1,73 +1,72 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   modifyNodeAction,
-  setActiveNodeAction
-} from "../../redux/actions/nodeActions";
+  setActiveNodeAction,
+} from '../../redux/actions/nodeActions'
 
 const TreeNode = props => {
-  const { nodeId, onPositionUpdate } = props;
-  let selectedNode = null;
-  let dragStartX = "";
-  let dragStartY = "";
-  const dispatch = useDispatch();
+  const { nodeId, onPositionUpdate } = props
+  let selectedNode = null
+  let dragStartX = ''
+  let dragStartY = ''
+  const dispatch = useDispatch()
   const { activeNodeID, connections } = useSelector(state => ({
     activeNodeID: state.nodeReducer.activeNodeID,
-    connections: state.connectorReducer.connections
-  }));
+    connections: state.connectorReducer.connections,
+  }))
 
   const onClickDown = event => {
-    const { clientX, clientY, target } = event;
-    dragStartX = clientX;
-    dragStartY = clientY;
-    selectedNode = target;
-  };
+    const { clientX, clientY, target } = event
+    dragStartX = clientX
+    dragStartY = clientY
+    selectedNode = target
+  }
 
   const onClickDrag = e => {
     if (selectedNode) {
       var dx =
-        parseInt(selectedNode.getAttribute("cx")) + e.clientX - dragStartX;
+        parseInt(selectedNode.getAttribute('cx')) + e.clientX - dragStartX
       var dy =
-        parseInt(selectedNode.getAttribute("cy")) + e.clientY - dragStartY;
-      dragStartX = e.clientX;
-      dragStartY = e.clientY;
-      selectedNode.setAttribute("cx", dx);
-      selectedNode.setAttribute("cy", dy);
-      onPositionUpdate(nodeId, dx, dy);
+        parseInt(selectedNode.getAttribute('cy')) + e.clientY - dragStartY
+      dragStartX = e.clientX
+      dragStartY = e.clientY
+      selectedNode.setAttribute('cx', dx)
+      selectedNode.setAttribute('cy', dy)
     }
-  };
+  }
 
   const onClickUp = () => {
-    let newCx = selectedNode.getAttribute("cx");
-    let newCy = selectedNode.getAttribute("cy");
-    let payload = { cx: Number(newCx), cy: Number(newCy), id: nodeId };
-    dispatch(modifyNodeAction(payload));
-    selectedNode = null;
-  };
+    let newCx = selectedNode.getAttribute('cx')
+    let newCy = selectedNode.getAttribute('cy')
+    let payload = { cx: Number(newCx), cy: Number(newCy), id: nodeId }
+    dispatch(modifyNodeAction(payload))
+    selectedNode = null
+  }
 
   const setActiveNode = event => {
-    event.stopPropagation();
-    dispatch(setActiveNodeAction(nodeId));
-  };
+    event.stopPropagation()
+    dispatch(setActiveNodeAction(nodeId))
+  }
 
-  let newProps = Object.assign({}, props);
-  newProps.nodeId && delete newProps.nodeId;
-  newProps.onPositionUpdate && delete newProps.onPositionUpdate;
+  let newProps = Object.assign({}, props)
+  newProps.nodeId && delete newProps.nodeId
+  newProps.onPositionUpdate && delete newProps.onPositionUpdate
 
   return (
     <circle
-      className="tree-node"
+      className='tree-node'
       onMouseDown={onClickDown}
       onMouseMove={onClickDrag}
       onMouseUp={onClickUp}
       onClick={setActiveNode}
       r={activeNodeID === nodeId ? 50 : 30}
-      fill={activeNodeID === nodeId ? "lightblue" : "white"}
-      strokeWidth="3"
-      stroke={"black"}
+      fill={activeNodeID === nodeId ? 'lightblue' : 'white'}
+      strokeWidth='3'
+      stroke={'black'}
       {...newProps}
     ></circle>
-  );
-};
+  )
+}
 
-export default TreeNode;
+export default TreeNode
